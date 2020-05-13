fdcbe12ab9c937324fe1d49115e19d2573438c56
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-13 20:01:53 +0530

    startGame function stored to variable start

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame () {
+const start = function startGame () {
     console.log('Starting Game')
 }
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', start);
