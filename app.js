fc28ef79abc0eba2da091dde5643cf0ae35998ee
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-15 22:59:33 +0530

    selection added - ROCK, PAPER, SCISSORS

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+
+const getPlayerChoice = function() {
+    const selection = prompt('Rock, Paper or Scissors?');
+
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert('Invalid Choice! We chose Rock for you')
+    }
+}
+
 const start = () => {
     console.log('Starting Game')
 }
